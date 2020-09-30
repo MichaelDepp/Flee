e25d3b2e3ddb59beb710db872aa1ee834b3a0be5
MichaelDepp <michaeldepp7@gmail.com>
2020-09-30 21:40:34 +0800

    navclr

--- components/Navscreen.js
+++ components/Navscreen.js
@@ -5,33 +5,34 @@ function Navscreen() {
     const { colorMode, toggleColorMode } = useColorMode()
     const bg = useColorModeValue("#00102A", "#F8F8F8")
     const color = useColorModeValue("#F8F8F8", "#00102A")
-    const btn = "#304FFF"
+    const sbtn = useColorModeValue("#F8F8F8", "#304FFF")
+    const navclr = useColorModeValue("rgba(0, 16, 42, 0.9)", "rgba(248, 248, 248, 0.9)")
     return (
-        <Center minH="100vh" position="fixed" w="100vw" bg={bg}>
+        <Center minH="100vh" position="fixed" w="100vw" bg={navclr}>
             <Box justify="center" mb="200px">
                 <VStack>
                     <Link href="/">
-                    <Text fontWeight="black" fontSize={["5xl", "5xl", "6xl", "6xl"]}>
+                    <Text fontWeight="black" color={sbtn} fontSize={["5xl", "5xl", "6xl", "6xl"]}>
                         Home
                     </Text>
                     </Link>
                     <Link href="/lesson">
-                    <Text fontWeight="black" fontSize={["5xl", "5xl", "6xl", "6xl"]}>
+                    <Text fontWeight="black" color={sbtn} fontSize={["5xl", "5xl", "6xl", "6xl"]}>
                         Lesson
                     </Text>
                     </Link>
                     <Link href="/counselling">
-                    <Text fontWeight="black" fontSize={["5xl", "5xl", "6xl", "6xl"]}>
+                    <Text fontWeight="black" color={sbtn} fontSize={["5xl", "5xl", "6xl", "6xl"]}>
                         Counselling
                     </Text>
                     </Link>
                     <Link href="/forum">
-                    <Text fontWeight="black" fontSize={["5xl", "5xl", "6xl", "6xl"]}>
+                    <Text fontWeight="black" color={sbtn} fontSize={["5xl", "5xl", "6xl", "6xl"]}>
                         Forum
                     </Text>
                     </Link>
                     <Link href="/ministry">
-                    <Text fontWeight="black" fontSize={["5xl", "5xl", "6xl", "6xl"]}>
+                    <Text fontWeight="black" color={sbtn} fontSize={["5xl", "5xl", "6xl", "6xl"]}>
                         Ministry
                     </Text>
                     </Link>
