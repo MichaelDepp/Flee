11e64845c369f836dac6af2e72d5f323bed8163d
MichaelDepp <michaeldepp7@gmail.com>
2020-10-10 18:27:17 +0800

    Spinenr styling

--- components/Spinner.js
+++ components/Spinner.js
@@ -1,10 +1,12 @@
 import { CubeGrid } from "styled-loaders-react";
-import {Box}  from "@chakra-ui/core"
+import { Box } from "@chakra-ui/core";
 
 const Spinner = () => (
-    <Box>
-        <CubeGrid color="#304FFF" />
-    </Box>
+  <div className="loader">
+    <div className="cube">
+      <CubeGrid color="#304FFF" />
+    </div>
+  </div>
 );
 
-export default Spinner
+export default Spinner;
